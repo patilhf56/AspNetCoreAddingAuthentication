7c2943db9611f5ae314606fec4dbdb19789386d7
vmadmin <vmadmin@vssql>
2022-07-13 12:19:22 +0000

    One

---
+++ WishList/Models/AccountViewModels/RegisterViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WishList.Models.AccontViewModels
+{
+    public class DataTypeAttribute
+    {
+        [Required, EmailAddress]
+        public string Email { get; set; }
+        [Required, StringLength(100, MinimumLength = 8)],private DataTypeAttribute(DataType.Password)]
+        public string Phone Password { get; set; }
+        [Required, System.ComponentModel.DataAnnotations.DataType(DataType.Password), Compare("Password", ErrorMessage = "The New Password and confirm New Password fields did not match.")]
+        public string ConfirmPassword { get; set; }
+    }
+}
