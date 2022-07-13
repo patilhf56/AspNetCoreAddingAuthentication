0d9a39766c79391854861ca98984a239dce9ff88
vmadmin <vmadmin@vssql>
2022-07-13 13:13:09 +0000

    hd

--- WishList/Controllers/AccountController.cs
+++ WishList/Controllers/AccountController.cs
@@ -1,8 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Authorization;
+﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 using WishList.Models;
-
+using WishList.Models.AccountViewModels;
 namespace WishList.Controllers
 {
     [Authorize]
@@ -10,14 +10,36 @@ namespace WishList.Controllers
     {
         private readonly UserManager<ApplicationUser> _userManager;
         private readonly SignInManager<ApplicationUser> _signInManager;
+
         public AccountController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager)
         {
             _userManager = userManager;
             _signInManager = signInManager;
         }
-        public IActionResult Index()
+        [HttpGet]
+        [AllowAnonymous]
+        public IActionResult Register()
         {
             return View();
+        }
+        [HttpPost]
+        [AllowAnonymous]
+        public IActionResult Register(RegisterViewModel model)
+        {
+            if (!ModelState.IsValid)
+                return View(model);
+            var result = _userManager.CreateAsync(new ApplicationUser() { Email = model.Email, UserName = model.Email }, model.Password).Result;
+
+            if (!result.Succeeded)
+            {
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError("Password", error.Description);
+                }
+                return View(model);
+            }
+
+            return RedirectToAction("Index", "Home");
         }
     }
 }
